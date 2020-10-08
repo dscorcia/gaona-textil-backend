7fcd0420d3de3aea7368df755058342ec0c88e55
Damian Scorciapino <damian_scorciapino@hotmail.com>
2020-10-08 16:58:15 -0300

    valido usuario existente y encripto passw en DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,43 @@
 const express = require ('express');
 const {validationResult} = require ('express-validator');
+const Usuario = require('../models/Usuario')
+const bcrypt = require('bcryptjs');
 
 
-const crearUsuario = (req,res = express.response)=>{
+const crearUsuario = async (req,res = express.response)=>{
 
     const {name,password,empresa} = req.body
-   
-       
+try {
+    
+    let usuario = await Usuario.findOne({name})
+    if(usuario){
+        return res.status(400).json({
+            ok: false,
+            msg:'Ya existe ese nombre de usuario'
+        })
+    }
+
+     usuario =  new Usuario(req.body);
+
+     //Encriptar contraseña
+     const salt = bcrypt.genSaltSync();
+     usuario.password = bcrypt.hashSync(password,salt);
+    await usuario.save();
+         
     res.status(201).json({
         ok:true,
-        msg:'Registro',
-        name,
-        password,
-        empresa
+        msg:usuario.id,
+        name: usuario.name
+        
     })
+
+}catch(error) {
+    console.log(error)
+    res.status(500).json({
+        ok: false,
+        msg:'Hable con el administrador, no se creó el usuario'
+    });
+    }
 
 }
 
