873f557ae0352d4e2f3d9a2fb46a0b2271a612ec
Damian Scorciapino <damian_scorciapino@hotmail.com>
2020-10-09 01:41:36 -0300

    revalido JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -97,13 +97,19 @@ const loginUsuario =  async (req,res = express.response)=>{
 }
 
 
-const revalidarToken = (req,res = express.response)=>{
+const revalidarToken = async (req,res = express.response)=>{
+
+    const uid = req.uid;
+    const name = req.name;
+
+
+    const token = await generarJWT(uid, name)
 
     res.json({
         ok:true,
-        msg:'Renew'
+        token
     })
-
+    
 }
 
 module.exports ={
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const {check} = require('express-validator');
 const {validarCampos} = require('../middlewares/validar-campos');
 const router = Router();
 const {crearUsuario, loginUsuario, renewToken: revalidarToken} = require('../controllers/auth');
+const {validarJWT} = require('../middlewares/validar-jwt');
 
 
 
@@ -29,7 +30,7 @@ router.post(
     ],
     loginUsuario);
 
-router.get('/renew',revalidarToken);
+router.get('/renew',validarJWT,revalidarToken);
 
 
 module.exports = router;
