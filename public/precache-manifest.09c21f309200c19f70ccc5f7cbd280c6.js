b185fd378e108f186fdbc4947245de386c8474cf
Damian Scorciapino <damian_scorciapino@hotmail.com>
2021-01-25 17:19:00 -0300

    v1 gaonaApp

---
+++ public/precache-manifest.09c21f309200c19f70ccc5f7cbd280c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bed117af689526dbd98faad2c21d7fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca90381ed1ae1d9820e",
+    "url": "/static/css/2.63343480.chunk.css"
+  },
+  {
+    "revision": "eb91a040ef3c54ec0131",
+    "url": "/static/css/main.9c2a31b3.chunk.css"
+  },
+  {
+    "revision": "6ca90381ed1ae1d9820e",
+    "url": "/static/js/2.e87e4ed9.chunk.js"
+  },
+  {
+    "revision": "98c92e4a71422a59c3daed74f16a0206",
+    "url": "/static/js/2.e87e4ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb91a040ef3c54ec0131",
+    "url": "/static/js/main.65ce67c9.chunk.js"
+  },
+  {
+    "revision": "0cb516cdc09d15f00029",
+    "url": "/static/js/runtime-main.d74c7a97.js"
+  },
+  {
+    "revision": "743363c821ffeb8f01505fdf3c5b924a",
+    "url": "/static/media/rollos.743363c8.jpg"
+  }
+]);
