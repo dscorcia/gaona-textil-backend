eb9f8e44663938454e40e53c94cba1001b7539b6
Damian Scorciapino <damian_scorciapino@hotmail.com>
2021-02-12 01:21:14 -0300

    deploy heroku con cantidades stock

---
+++ public/precache-manifest.6c53def496d94919ad39225fbec4eec1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9674e31f2bf5f2184d997ff285dbc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca90381ed1ae1d9820e",
+    "url": "/static/css/2.63343480.chunk.css"
+  },
+  {
+    "revision": "373dbed54b713df18dd2",
+    "url": "/static/css/main.9c2a31b3.chunk.css"
+  },
+  {
+    "revision": "6ca90381ed1ae1d9820e",
+    "url": "/static/js/2.e87e4ed9.chunk.js"
+  },
+  {
+    "revision": "98c92e4a71422a59c3daed74f16a0206",
+    "url": "/static/js/2.e87e4ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373dbed54b713df18dd2",
+    "url": "/static/js/main.24b43329.chunk.js"
+  },
+  {
+    "revision": "0cb516cdc09d15f00029",
+    "url": "/static/js/runtime-main.d74c7a97.js"
+  },
+  {
+    "revision": "743363c821ffeb8f01505fdf3c5b924a",
+    "url": "/static/media/rollos.743363c8.jpg"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1ee61421ad0114a05c6307f1d970ded.js"
+  "/precache-manifest.6c53def496d94919ad39225fbec4eec1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
